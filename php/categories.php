8ad423f0bab4245b262e4c3edab94b5be4aeaa14
smile <zgcmhx@126.com>
2016-03-10 19:06:27 +0800

    添加了init.php

--- php/categories.php
+++ php/categories.php
@@ -1,25 +1,30 @@
 <?php
+require ('init.php');
+
 header("content-Type:text/html;charset=utf-8");
 session_start();
-//var_dump($_GET);
 
-//echo $_GET['searchText'];
 $term= isset($_GET['searchText'])? $_GET['searchText'] : '';
-//echo $term;
-    $con = mysql_connect("localhost:3306", "root", "");
+$type= isset($_GET['searchType'])? $_GET['searchType'] : '';
+
+    $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
     mysql_set_charset("utf8", $con);
 
     if (!$con) {
         die('连接失败 ' . mysql_error());
     } else {
-        mysql_select_db("cucmooc", $con);
+        mysql_select_db(DB_NAME, $con);
         //echo("链接数据库成功");
 
 //搜索课程or知识点
-        $sql = "select courseName,courseDesc from lesson where courseName like '%$term%'";
+        if (!$type){
+            echo 'error';
+        }
+       else if($type=='course'){
+        $sql = "select courseName,courseDesc from course where courseName like '%$term%'";
         $result = mysql_query($sql);
         if (!$result) {
-            echo "<script>alert('没有此课程或知识点！');//</script>";
+            echo "<script>alert('没有此课程！');//</script>";
             mysql_close($con);
         } else {
             $arr = array();
@@ -30,6 +35,23 @@ $term= isset($_GET['searchText'])? $_GET['searchText'] : '';
         }
 
         mysql_close($con);
+        }
+       else if($type=='point'){
+            $sql = "select pointName,pointDesc from point where pointName like '%$term%'";
+            $result = mysql_query($sql);
+            if (!$result) {
+                echo 'error';
+                mysql_close($con);
+            } else {
+                $arr = array();
+                while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+                    $arr[] = $row;
+                }
+                echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+            }
+
+            mysql_close($con);
+        }
     }//链接数据库成功
 
 ?>
